7123e13d4c6dd19859a3b4ab5239c3a91bcc7fec
tofu159mac <tofu159mac@outlook.com>
2025-08-05 11:40:21 +0530

    Create ExpenseList.js

---
+++ src/components/ExpenseList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ExpenseList.css';
+
+const ExpenseList = () => {
+  return (
+    <div className="expense-list">
+      <p>No expenses yet.</p>
+    </div>
+  );
+};
+
+export default ExpenseList;
